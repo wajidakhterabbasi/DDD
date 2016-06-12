356c69c854709082f106e935d195e131d211de41
wajid abbasi <wajidabbasi@wajids-mbp.home>
2016-06-12 04:24:20 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+wajid.dev
+=========
+
+A Symfony project created on June 2, 2016, 9:42 pm.
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+interface eMailBody
+{
+    public function loadBody();
+}
+class eMail implements eMailBody
+{
+    public function loadBody()
+    {
+        echo "This is Main Email body.<br />";
+    }
+}
+abstract class emailBodyDecorator implements eMailBody
+{
+    protected $emailBody;
+    public function __construct(eMailBody $emailBody)
+    {
+        $this->emailBody = $emailBody;
+    }
+    abstract public function loadBody();
+}
+class christmasEmailBody extends emailBodyDecorator
+{
+    public function loadBody()
+    {
+        echo 'This is Extra Content for Christmas<br />';
+        $this->emailBody->loadBody();
+    }
+}
+
+class newYearEmailBody extends emailBodyDecorator
+{
+    public function loadBody()
+    {
+        echo 'This is Extra Content for New Year.<br />';
+        $this->emailBody->loadBody();
+    }
+}
+
+$email = new newYearEmailBody(new christmasEmailBody(new eMail()));
+$email->loadBody();
+
+$email = new eMail();
+$email = new newYearEmailBody($email);
+$email->loadBody();
+$email = new eMail();
+$email = new christmasEmailBody($email);
+$email = new newYearEmailBody($email);
+$email->loadBody();
